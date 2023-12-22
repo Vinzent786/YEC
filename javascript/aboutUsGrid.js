01ae9d3fb1b540223fe01ce10d9824d48846cc9e
Vinzent786 <vincentcook6864@gmail.com>
2023-12-22 15:13:13 -0500

    Updating mobile grid layout

--- javascript/aboutUsGrid.js
+++ javascript/aboutUsGrid.js
@@ -6,27 +6,35 @@ function buildMobileGrid() {
     mobileGrid.appendTo($('#about-us'));
     (() => {
         const article1 = $(`<article id="mobile-article1"></article>`);
-        $('<h2>The Room of Prestige!</h2>').appendTo(article1);
-        $('<p>Come and let your imagination free by creating the experience YOU desire!</p>').appendTo(article1);
-        $('<div></div>').appendTo(article1);
+        const infoDiv1 = $('<div class="info-div"></div>');
+        $('<h2>The Room of Prestige!</h2>').appendTo(infoDiv1);
+        $('<p>Come and let your imagination free by creating the experience YOU desire!</p>').appendTo(infoDiv1);
+        $(infoDiv1).appendTo(article1);
+        $('<div class="grid-img"></div>').appendTo(article1);
         $(article1).appendTo(mobileGrid);
 
         const article2 = $(`<article id="mobile-article2"></article>`);
-        $('<h2>This is the place for YOU!</h2>').appendTo(article2);
-        $('<p>Have the VIP experience for your guest with convenient secure parking!</p>').appendTo(article2);
-        $('<div></div>').appendTo(article2);
+        const infoDiv2 = $('<div class="info-div"></div>');
+        $('<h2>This is the place for YOU!</h2>').appendTo(infoDiv2);
+        $('<p>Have the VIP experience for your guest with convenient secure parking!</p>').appendTo(infoDiv2);
+        $(infoDiv2).appendTo(article2);
+        $('<div class="grid-img"></div>').appendTo(article2);
         $(article2).appendTo(mobileGrid);
 
         const article3 = $(`<article id="mobile-article3"></article>`);
-        $('<h2>We would LOVE to host YOU!</h2>').appendTo(article3);
-        $('<p>Host your next VIP event HERE!</p>').appendTo(article3);
-        $('<div></div>').appendTo(article3);
+        const infoDiv3 = $('<div class="info-div"></div>');
+        $('<h2>We would LOVE to host YOU!</h2>').appendTo(infoDiv3);
+        $('<p>Host your next VIP event HERE!</p>').appendTo(infoDiv3);
+        $(infoDiv3).appendTo(article3);
+        $('<div class="grid-img"></div>').appendTo(article3);
         $(article3).appendTo(mobileGrid);
 
         const article4 = $(`<article id="mobile-article4"></article>`);
-        $('<h2>We would LOVE to host YOU!</h2>').appendTo(article4);
-        $('<p>Host your next VIP event HERE!</p>').appendTo(article4);
-        $('<div></div>').appendTo(article4);
+        const infoDiv4 = $('<div class="info-div"></div>');
+        $('<h2>We would LOVE to host YOU!</h2>').appendTo(infoDiv4);
+        $('<p>Host your next VIP event HERE!</p>').appendTo(infoDiv4);
+        $(infoDiv4).appendTo(article4);
+        $('<div class="grid-img"></div>').appendTo(article4);
         $(article4).appendTo(mobileGrid);
     })();
 }
